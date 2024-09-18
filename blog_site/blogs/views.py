4ff657aab6252c9817d5aa7d8e1545307947ca40
eli <ebezhiasvhili@gmail.com>
2024-09-18 16:38:42 +0400

    add mock_data for blogs

--- blog_site/blogs/views.py
+++ blog_site/blogs/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from mock_data import blogs
 
-# Create your views here.
+def blogs_view(request):
+    html = ""
+    for blog in blogs.values():
+        html += f"""
+        <h3>{blog['title']}   
+        <p>{blog['author']} on {blog['date_published']}<p>
+        <p>{blog['content']}<p>
+        <p>Tags: {', '.join(blog['tags'])}</p>
+        """
+    return HttpResponse(html, status=200)
