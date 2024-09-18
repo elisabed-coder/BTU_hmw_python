e0fd301dc3562af9598cfc2dbd8309eb4b12aa11
eli <ebezhiasvhili@gmail.com>
2024-09-18 16:51:21 +0400

    add blogs

--- blog_site/blog_site/urls.py
+++ blog_site/blog_site/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+
 from blogs.views import blogs_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blogs/', blogs_view)
+    path('blogs/', blogs_view),
 ]
--- blog_site/blogs/views.py
+++ blog_site/blogs/views.py
@@ -1,10 +1,12 @@
 from mock_data import blogs
+from django.http import HttpResponse
+
 
 def blogs_view(request):
     html = ""
     for blog in blogs.values():
         html += f"""
-        <h3>{blog['title']}   
+        <h3>{blog['title']}</h3>
         <p>{blog['author']} on {blog['date_published']}<p>
         <p>{blog['content']}<p>
         <p>Tags: {', '.join(blog['tags'])}</p>
