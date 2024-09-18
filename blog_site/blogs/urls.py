940741efb48580157224f2c1ab2424a6e4ac16b6
eli <ebezhiasvhili@gmail.com>
2024-09-18 18:59:53 +0400

    add blogs/urls

---
+++ blog_site/blogs/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+from blogs.views import blogs_view, blog_details_view
+
+urlpatterns = [
+    path('', blogs_view, name='blogs'),
+    path('<int:blog_key>/', blog_details_view, name='blog-details'),
+]
--- blog_site/blogs/views.py
+++ blog_site/blogs/views.py
@@ -1,14 +1,34 @@
-from mock_data import blogs
 from django.http import HttpResponse
+from django.urls import reverse
+
+from mock_data import blogs
 
 
 def blogs_view(request):
     html = ""
-    for blog in blogs.values():
+    for key, blog in blogs.items():
+        blog_details_url = reverse("blog-details", args=(key,))
         html += f"""
         <h3>{blog['title']}</h3>
-        <p>{blog['author']} on {blog['date_published']}<p>
-        <p>{blog['content']}<p>
+        <p>By {blog['author']} on {blog["date_published"]}</p>
+        <p>{blog['content']}</p>
         <p>Tags: {', '.join(blog['tags'])}</p>
+        <a href={blog_details_url}>View Details</a>
         """
-    return HttpResponse(html, status=200)
+    return HttpResponse(html)  # This ensures the correct response type
+
+def blog_details_view(request, blog_key):
+    blog = blogs[blog_key]
+    comments = ""
+    for comment in blog['comments']:
+        comments += f"<p>{comment['author']}: {comment['content']} <small>{comment['date']}</small></p>"
+    html = f"""
+    <h3>Blog Details</h3>
+    <h2>{blog['title']}</h3>
+    <p>By {blog['author']} on {blog["date_published"]}</p>
+    <p>{blog['content']}</p>
+    <p>Tags: {', '.join(blog['tags'])}</p>
+    <hr>
+    <p>{comments}</p>
+    """
+    return HttpResponse(html)
